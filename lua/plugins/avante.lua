494ba4b247a0e90029d055c66a333cfc2242fcae
ARoennau <stillalive1025@gmail.com>
2025-11-29 18:22:01 -0500

    fixes

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,23 +1,24 @@
-return {
-  "yetone/avante.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "stevearc/dressing.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-    "MeanderingProgrammer/render-markdown.nvim",
-  },
-  opts = {
-    provider = "copilot", -- or "openai", "anthropic" etc.
-  },
-  keys = {
-    {
-      "<leader>a",
-      function()
-        require("avante").toggle() -- example toggle or invoke command
-      end,
-      desc = "Toggle Avante AI Assistant",
-    },
-  },
-}
+return {}
+-- return {
+--   "yetone/avante.nvim",
+--   event = "VeryLazy",
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--     "stevearc/dressing.nvim",
+--     "nvim-tree/nvim-web-devicons",
+--     "MunifTanjim/nui.nvim",
+--     "MeanderingProgrammer/render-markdown.nvim",
+--   },
+--   opts = {
+--     provider = "copilot", -- or "openai", "anthropic" etc.
+--   },
+--   keys = {
+--     {
+--       "<leader>a",
+--       function()
+--         require("avante").toggle() -- example toggle or invoke command
+--       end,
+--       desc = "Toggle Avante AI Assistant",
+--     },
+--   },
+-- }
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -86,6 +86,8 @@ return {
       mapping = cmp.mapping.preset.insert({
         ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
@@ -98,6 +100,7 @@ return {
       }),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
+        { name = "copilot" },
         { name = "luasnip" },
         { name = "buffer" },
         { name = "path" },
